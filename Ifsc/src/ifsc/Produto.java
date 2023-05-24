dd5635e2a62767892116eac988ac3b680a53c1d1
AndreiLud7 <124809787+AndreiLud7@users.noreply.github.com>
2023-05-24 11:25:37 -0600

    Add files via upload

---
+++ Ifsc/src/ifsc/Produto.java
@@ -0,0 +1,38 @@
+package ifsc;
+
+public class Produto {
+
+	private String nome;
+	private Double preco;
+	private String Fornecedor;
+	private Long CodBarras;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	public String getFornecedor() {
+		return Fornecedor;
+	}
+	public void setFornecedor(String fornecedor) {
+		Fornecedor = fornecedor;
+	}
+	public Long getCodBarras() {
+		return CodBarras;
+	}
+	public void setCodBarras(Long codBarras) {
+		CodBarras = codBarras;
+	}
+	
+	
+	
+	
+	
+}
